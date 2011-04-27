ee0654542dfd7cd1421b8a8ea783a0f8245a0ee6
Philipp Wassibauer <phil@maptales.com>
2011-04-27 21:01:42 +0200

    final fixes

--- company/settings.py
+++ company/settings.py
@@ -60,7 +60,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'biotechsender@gmail.com'
 EMAIL_HOST_PASSWORD = 'bio*tech'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 
 DEFAULT_FROM_EMAIL = "biotechsender@gmail.com"
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
